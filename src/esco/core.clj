f3d2a7ebc7e9fad7a4d863f7d1c5d8112f06bc4a
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2019-02-01 20:25:13 +0200

    Initial commit.

---
+++ src/esco/core.clj
@@ -0,0 +1,6 @@
+(ns esco.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/esco/core_test.clj
@@ -0,0 +1,7 @@
+(ns esco.core-test
+  (:require [clojure.test :refer :all]
+            [esco.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
